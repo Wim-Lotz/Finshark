d98e9c81e53828112294a2392aece9170aa8d395
Wim-Lotz <wimlotz@protonmail.com>
2024-06-01 14:21:01 +0200

    added user manager

---
+++ api/Migrations/20240601121653_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "6e3f4d25-b4e6-498a-ba92-2913d1b199c8", null, "Admin", "ADMIN" },
+                    { "e5ae892e-2c09-408a-b3e0-83be9624576a", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "6e3f4d25-b4e6-498a-ba92-2913d1b199c8");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e5ae892e-2c09-408a-b3e0-83be9624576a");
+        }
+    }
+}
